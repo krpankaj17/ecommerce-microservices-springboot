79f19829f6567ba45f918a8a44e4eb23b7f7a7d2
krpankaj17 <pankajsinghshekhawat752@gmail.com>
2026-05-13 09:36:31 +0530

    Updated Readme

--- product-service/src/main/java/com/pankaj/ecommerce/product_service/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/pankaj/ecommerce/product_service/service/ProductServiceImpl.java
@@ -11,8 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Service
 public class ProductServiceImpl implements ProductService{
